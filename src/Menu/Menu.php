8c0facfb0feac573b4602a173ba3b15fefb66c0e
Tom Kay <oridan82@gmail.com>
2019-02-27 17:19:20 +0000

    add menu class to menu

--- src/Menu/Menu.php
+++ src/Menu/Menu.php
@@ -1,9 +1,15 @@
 <?php
 namespace PackagedUi\Elegance\Menu;
 
+use Packaged\Glimpse\Core\HtmlTag;
 use PackagedUi\Elegance\Lists\ListBox;
 
 class Menu extends ListBox
 {
   protected $_tag = 'nav';
+
+  protected function _prepareForProduce(): HtmlTag
+  {
+    return parent::_prepareForProduce()->addClass('menu');
+  }
 }
