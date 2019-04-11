a73593c8ae6e8d50c0ddaaa946e7369ba6cbcca8
Brooke Bryan <brooke@bajb.net>
2019-04-11 14:40:15 +0100

    Add require helpers on fusion

--- src/Fusion.php
+++ src/Fusion.php
@@ -17,4 +17,14 @@ class Fusion implements LayoutInterface, TypographyInterface, ButtonInferface, B
       'https://fonts.googleapis.com/css?family=' . $family . ':300,300i,400,400i,500,500i,700,700i,900'
     );
   }
+
+  public static function requireCss()
+  {
+    ResourceManager::componentClass(static::class)->requireCss(self::FILE_BASE_CSS);
+  }
+
+  public static function requireJs()
+  {
+    ResourceManager::componentClass(static::class)->requireJs(self::FILE_BASE_JS);
+  }
 }
