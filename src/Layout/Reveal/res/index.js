1fa0496e2355dc41d3c159b3eb91b906fd5b2371
Kyle Essex <kylegavinessex@hotmail.com>
2020-10-06 15:02:02 +0100

    - reveal icon toggle

--- src/Layout/Reveal/res/index.js
+++ src/Layout/Reveal/res/index.js
@@ -5,21 +5,23 @@ on(document, 'click', '[reveal-launcher]', (e) => {
   const targetId = e.delegateTarget.getAttribute('reveal-launcher');
   const reveal = document.querySelector(`#${targetId}`);
 
-  const launchers = document.querySelectorAll('[reveal-launcher=' + targetId + ']');
-
   if(reveal)
   {
     const show = reveal.classList.toggle('show');
-    launchers.forEach(ele => ele.toggleAttribute('reveal-open', show));
+    const launchers = document.querySelectorAll(`[reveal-launcher="${targetId}"]`);
+
+    if(reveal.hasAttribute('reveal-destructive'))
+    {
+      launchers.forEach(ele => ele.parentNode.removeChild(ele));
+    }
+    else
+    {
+      launchers.forEach(ele => ele.toggleAttribute('reveal-open', show));
+    }
   }
   else
   {
     console.warn('Cannot reveal. The target does not exist');
   }
 
-  if(reveal.hasAttribute('reveal-destructive'))
-  {
-    document.querySelectorAll(`[reveal-launcher="${targetId}"]`)
-            .forEach(ele => ele.parentNode.removeChild(ele));
-  }
 });
