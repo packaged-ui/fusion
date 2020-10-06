635d16e0cf051cc26e7a85258090faa0e373f415
Kyle Essex <kylegavinessex@hotmail.com>
2020-10-06 15:17:18 +0100

    - reveal icon toggle (#11)
    * - reveal icon toggle
    
    * - reveal icon toggle
    
    * - reveal icon toggle
    
    * Update src/Layout/Reveal/res/index.js
    
    * getElementById
    
    Co-authored-by: Tom Kay <oridan82@gmail.com>

--- demo/src/Elements/RevealDemo.php
+++ demo/src/Elements/RevealDemo.php
@@ -2,9 +2,12 @@
 
 namespace PackagedUi\FusionDemo\Elements;
 
+use Packaged\Dispatch\ResourceManager;
 use Packaged\Glimpse\Tags\HorizontalRule;
+use Packaged\Glimpse\Tags\Text\Paragraph;
 use PackagedUi\FontAwesome\FaIcon;
 use PackagedUi\Fusion\Button\Button;
+use PackagedUi\Fusion\Layout\Flex;
 use PackagedUi\Fusion\Layout\Reveal\Reveal;
 use PackagedUi\FusionDemo\AbstractDemoPage;
 
@@ -20,17 +23,58 @@ class RevealDemo extends AbstractDemoPage
     return FaIcon::HIGHLIGHTER;
   }
 
+  public function __construct()
+  {
+    ResourceManager::inline()->requireCss(
+      '[reveal-launcher] > [close],
+[reveal-launcher][reveal-open] > [open] {
+  display: none;
+}
+
+[reveal-launcher][reveal-open] > [close] {
+  display: block !important;
+}'
+    );
+  }
+
   protected function _content(): array
   {
     $toggleReveal = Reveal::create('some content');
     $destructiveReveal = Reveal::create('some content')->destructive();
     return [
-      $toggleReveal->applyLauncher(Button::create('toggle it')),
+      $toggleReveal->applyLauncher(
+        Flex::create(
+          Button::create('toggle it'),
+          Paragraph::create('OPEN')->setAttribute('open', true),
+          Paragraph::create('CLOSE')->setAttribute('close', true)
+        )
+      ),
+      $toggleReveal->applyLauncher(
+        Flex::create(
+          Button::create('toggle it'),
+          Paragraph::create('OPEN')->setAttribute('open', true),
+          Paragraph::create('CLOSE')->setAttribute('close', true)
+        )
+      ),
+      $toggleReveal->applyLauncher(
+        Flex::create(
+          Button::create('toggle it'),
+          Paragraph::create('OPEN')->setAttribute('open', true),
+          Paragraph::create('CLOSE')->setAttribute('close', true)
+        )
+      ),
       $toggleReveal,
       HorizontalRule::create(),
       $destructiveReveal->applyLauncher(Button::create('show it')),
       $destructiveReveal,
       $destructiveReveal->applyLauncher(Button::create('show it again')),
+      $toggleReveal->applyLauncher(
+        Flex::create(
+          Button::create('toggle it'),
+          Paragraph::create('OPEN')->setAttribute('open', true),
+          Paragraph::create('CLOSE')->setAttribute('close', true)
+        )
+      ),
     ];
   }
 }
--- src/Layout/Reveal/res/index.js
+++ src/Layout/Reveal/res/index.js
@@ -1,21 +1,26 @@
 import './reveal.css';
 import {on} from '../../../Foundation/res';
 
-on(document, 'click', '[reveal-launcher]', (e) =>
-{
+on(document, 'click', '[reveal-launcher]', (e) => {
   const targetId = e.delegateTarget.getAttribute('reveal-launcher');
-  const reveal = document.querySelector(`#${targetId}`);
+  const reveal = document.getElementById(targetId);
+
   if(reveal)
   {
-    reveal.classList.toggle('show');
+    const show = reveal.classList.toggle('show');
+    const launchers = document.querySelectorAll(`[reveal-launcher="${targetId}"]`);
+
+    if(reveal.hasAttribute('reveal-destructive'))
+    {
+      launchers.forEach(ele => ele.parentNode.removeChild(ele));
+    }
+    else
+    {
+      launchers.forEach(ele => ele.toggleAttribute('reveal-open', show));
+    }
   }
   else
   {
     console.warn('Cannot reveal. The target does not exist');
-  }
-  if(reveal.hasAttribute('reveal-destructive'))
-  {
-    document.querySelectorAll(`[reveal-launcher="${targetId}"]`)
-            .forEach(ele => ele.parentNode.removeChild(ele));
   }
 });
