7a307a627f1e74830429601f1ebb9a6362f132f1
Kyle Essex <kylegavinessex@hotmail.com>
2020-12-04 11:21:21 +0000

    - add compiler options to minify further

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,35 @@ import {terser} from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
+import combineMediaQuery from 'postcss-combine-media-query';
+import cleanCss from 'postcss-clean';
 
 process.chdir(__dirname);
 
 const defaultCfg = {
-  input: './build.js',
-  output: {
-    file: './src/_resources/fusion.min.js',
+  input:   './build.js',
+  output:  {
+    file:   './src/_resources/fusion.min.js',
     format: 'iife',
   },
   plugins: [
     resolve({browser: true, preferBuiltins: false}),
     commonjs(),
     terser(),
-    postcss({extract: true, minimize: true}),
+    postcss({
+      extract:  true,
+      minimize: true,
+      plugins:  [
+        combineMediaQuery(),
+        cleanCss({
+          level: {
+            1: {
+              roundingPrecision: 3,
+            },
+          },
+        }),
+      ],
+    }),
   ],
 };
 
