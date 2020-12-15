eb0a1dfdbf80b975cd1f51ca51a20a82ef636c7c
Kyle Essex <kylegavinessex@hotmail.com>
2020-12-15 11:50:11 +0000

    - step wrapper fixes (#28)

--- src/VerticalStepper/res/index.js
+++ src/VerticalStepper/res/index.js
@@ -1,2 +1,37 @@
 import './step-wrapper.scss';
 import './step.scss';
+import {Pagelets} from '@packaged-ui/pagelets';
+import {onReady} from '../../Foundation/res';
+
+onReady((e) =>
+{
+  setLastStep();
+});
+
+document.addEventListener(Pagelets.events.COMPLETE, (e) =>
+{
+  setLastStep();
+});
+
+function setLastStep()
+{
+  const stepWrapper = document.querySelectorAll('.step-wrapper');
+
+  stepWrapper.forEach((elem, i) =>
+  {
+    let steps = elem.childNodes;
+
+    if (!steps)
+    {
+      return;
+    }
+
+    const length = steps.length - 1;
+    // If the last element doesnt contain anything and doesn't have a .step
+    if (!steps[length].hasChildNodes()
+        && !steps[length].classList.contains('step'))
+    {
+      steps[length - 1].classList.add('step--last');
+    }
+  });
+}
