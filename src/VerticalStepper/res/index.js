f7df2218b122daa3e539486076944b259ebbab66
Tom Kay <oridan82@gmail.com>
2021-01-15 10:01:38 +0000

    fix stepper issues

--- src/VerticalStepper/res/index.js
+++ src/VerticalStepper/res/index.js
@@ -3,27 +3,29 @@ import './step.scss';
 import {Pagelets} from '@packaged-ui/pagelets';
 import {onReady} from '../../Foundation/res';
 
-onReady((e) => {
-  setLastStep();
-});
+onReady(setLastStep());
 
-document.addEventListener(Pagelets.events.COMPLETE, (e) => {
-  setLastStep();
-});
-
-function setLastStep() {
-  const stepWrappers = document.querySelectorAll('.step-wrapper');
-
-  stepWrappers.forEach((stepWrapper, i) => {
-
-    let steps = stepWrapper.querySelectorAll('.step');
-
-    if(!steps)
+document.addEventListener(
+  Pagelets.events.COMPLETE, (e) =>
+  {
+    if(e.detail.request.getResolvedTarget.querySelector('.step-wrapper'))
     {
-      return;
+      setLastStep();
     }
+  }
+);
 
-    steps[steps.length - 1].classList.add('step--last');
-
-  });
+function setLastStep()
+{
+  document.querySelectorAll('.step-wrapper').forEach(
+    (stepWrapper) =>
+    {
+      stepWrapper.querySelectorAll('.step').forEach(
+        (step, i) =>
+        {
+          step.classList.toggle('step--last', i === steps.length - 1);
+        }
+      );
+    }
+  );
 }
