9205feb38dba1f506d3fb2f995d5c80314e70847
Brooke Bryan <brooke@bajb.net>
2021-07-07 15:26:33 +0100

    Add touch-started class

--- src/Foundation/res/index.js
+++ src/Foundation/res/index.js
@@ -5,11 +5,15 @@ export * from './events';
 
 onReadyState().then(() => document.body.classList.add('f-loaded'));
 
+if(navigator && navigator.maxTouchPoints > 0)
+{
+  document.body.classList.add('touch-enabled');
+}
+
 window.addEventListener(
   'touchstart',
-  function _fn()
-  {
-    document.body.classList.add('touch-enabled');
+  function _fn() {
+    document.body.classList.add('touch-enabled', 'touch-started');
     window.removeEventListener('touchstart', _fn, false);
   },
   false,
