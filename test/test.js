4fd105b09783b34c9e395c8337aa2e3a5094933a
Tom Kay <oridan82@gmail.com>
2021-07-19 18:20:34 +0100

    add (failing) unit tests for event

---
+++ test/test.js
@@ -0,0 +1,119 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import {on} from '../src/Foundation/res/events.js';
+
+chai.use(chaiAsPromised);
+
+describe(
+  'events', function ()
+  {
+    const eventName = 'test-event';
+
+    it(
+      'on - fail', function (done)
+      {
+        const {target} = _getElements();
+        const check = Math.random();
+        let fired = false;
+        on(
+          target, eventName, '.wrong-class', function ()
+          {
+            fired = true;
+            done('event fired, but should not have');
+          }
+        );
+        _dispatch(target, eventName, check);
+        setTimeout(() => fired || done(), 500);
+      }
+    );
+
+    it(
+      'on - target', function (done)
+      {
+        const {target} = _getElements();
+        const successClass = 'do-it';
+        const check = Math.random();
+        target.classList.add(successClass);
+        on(
+          target, eventName, '.' + successClass, function (e)
+          {
+            if(e.detail === check)
+            {
+              done();
+            }
+            else
+            {
+              done('event fired, but failed check');
+            }
+          }
+        );
+        _dispatch(target, eventName, check);
+      }
+    );
+
+    it(
+      'on - intermediate', function (done)
+      {
+        const {intermediate, target} = _getElements();
+        const successClass = 'do-it';
+        const check = Math.random();
+        target.classList.add(successClass);
+        on(
+          intermediate, eventName, '.' + successClass, function (e)
+          {
+            if(e.detail === check)
+            {
+              done();
+            }
+            else
+            {
+              done('event fired, but failed check');
+            }
+          }
+        );
+        _dispatch(target, eventName, check);
+      }
+    );
+
+    it(
+      'on - delegate', function (done)
+      {
+        const {delegate, target} = _getElements();
+        const successClass = 'do-it';
+        const check = Math.random();
+        target.classList.add(successClass);
+        on(
+          delegate, eventName, '.' + successClass, function (e)
+          {
+            if(e.detail === check)
+            {
+              done();
+            }
+            else
+            {
+              done('event fired, but failed check');
+            }
+          }
+        );
+        _dispatch(target, eventName, check);
+      }
+    );
+  }
+);
+
+function _getElements()
+{
+  const delegate = document.createElement('div');
+  const intermediate = document.createElement('div');
+  delegate.append(intermediate);
+  const target = document.createElement('div');
+  intermediate.append(target);
+  return {delegate, intermediate, target};
+}
+
+function _dispatch(ele, eventName, detail)
+{
+  return ele.dispatchEvent(
+    new CustomEvent(eventName, {bubbles: true, cancelable: true, composed: true, detail})
+  );
+}
