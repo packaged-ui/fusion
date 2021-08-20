0f2da671472ab6e5f1a18c81d0da7ab7d49188b4
Tom Kay <oridan82@gmail.com>
2021-08-20 16:38:56 +0100

    drop reliance on pagelets

--- src/VerticalStepper/res/init.js
+++ src/VerticalStepper/res/init.js
@@ -1,7 +1,6 @@
 import './step-wrapper.scss';
 import './step.scss';
 import {onReadyState} from '@packaged-ui/ready-promise';
-import {Pagelets} from '@packaged-ui/pagelets';
 
 let _init = new WeakSet();
 
@@ -14,21 +13,20 @@ export function init(rootElement = document)
   _init.add(rootElement);
 
   onReadyState().then(setLastStep);
-  rootElement.addEventListener(Pagelets.events.COMPLETE, setLastStep);
+}
 
-  function setLastStep()
-  {
-    rootElement.querySelectorAll('.step-wrapper').forEach(
-      (stepWrapper) =>
-      {
-        const steps = stepWrapper.querySelectorAll('.step');
-        steps.forEach(
-          (step, i) =>
-          {
-            step.classList.toggle('step--last', i === steps.length - 1);
-          }
-        );
-      }
-    );
-  }
+export function setLastStep()
+{
+  rootElement.querySelectorAll('.step-wrapper').forEach(
+    (stepWrapper) =>
+    {
+      const steps = stepWrapper.querySelectorAll('.step');
+      steps.forEach(
+        (step, i) =>
+        {
+          step.classList.toggle('step--last', i === steps.length - 1);
+        }
+      );
+    }
+  );
 }
